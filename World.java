2236c9ea7242cad7a8dc2b53af5905feb80fe687
Son,Haegwang <sonhaiguang21@gmail.com>
2024-10-16 09:29:37 +0900

    five

--- World.java
+++ World.java
@@ -3,5 +3,7 @@ public class World {
     public static void run() {
       System.out.println("201914112");
     }
-  
+    public static void run2() {
+        System.out.println("hello world");
+      }    
   }  
